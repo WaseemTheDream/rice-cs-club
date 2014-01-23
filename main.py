d1d31b146d85cab4e56927045f7c238e92e014d1
Waseem Ahmad <waseem@rice.edu>
2014-01-23 00:15:40 -0600

    Created a global.json file for configuring data and info for the website

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ __author__ = 'Waseem Ahmad (waseem@rice.edu)'
 
 
 import jinja2
+import json
 import logging
 import os
 import webapp2
@@ -17,25 +18,15 @@ PAGES_DIR = os.path.join(MAIN_DIR, 'pages')
 JINJA_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(PAGES_DIR))
 
-NAV_BAR = [
-    {'text': 'Home', 'link': 'index'},
-    {'text': 'Officers', 'link': 'officers'},
-    {'text': 'Sponsors', 'link': 'sponsors'},
-    {'text': 'Career', 'link': 'career'},
-    {'text': 'Events', 'link': 'events'},
-    {'text': 'Rice Apps', 'link': 'riceapps'},
-    {'text': 'Hack Rice', 'link': 'http://hack.rice.edu/'},
-    {'text': 'Contact', 'link': 'contact'}]
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        logging.info('Requested URL: %s', self.request.path)
+        template_vals = json.loads(open('global.json', 'r').read())
 
         # Get the page name being requested
         # assume index.html if none specified
         page_name = self.request.path
         if page_name == '/':
-            page_name = NAV_BAR[0]['link']
+            page_name = template_vals['nav_bar'][0]['link']
 
         # Get page info
         try:
@@ -43,9 +34,9 @@ class MainHandler(webapp2.RequestHandler):
         except Exception as e:
             page = JINJA_ENV.get_template('not_found.html').render()
 
+        template_vals['page_content'] = page
+
         template = JINJA_ENV.get_template('template.html')
-        template_vals = {'nav_bar': NAV_BAR,
-                         'page_content': page}
         self.response.out.write(template.render(template_vals))
 
 app = webapp2.WSGIApplication([('/.*', MainHandler)],
